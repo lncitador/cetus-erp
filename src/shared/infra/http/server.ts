ba6a50812291c9b166f95216f5e8a47d651b3da5
lncitador <walaffonofre@icloud.com>
2021-08-07 18:01:07 -0300

    🔧 configured to handle errors in express

--- src/shared/infra/http/server.ts
+++ src/shared/infra/http/server.ts
@@ -1,8 +1,28 @@
-import express from "express";
+import express, { Request, Response, NextFunction } from "express";
+import "express-async-errors";
+
+import { AppError } from "@shared/errors/AppError";
 
 const server = express();
 
 server.use(express.json());
+
+server.use(
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
+  (err: Error, _request: Request, response: Response, _next: NextFunction) => {
+    if (err instanceof AppError) {
+      return response.status(err.statusCode).json({
+        status: "error",
+        message: err.message,
+      });
+    }
+
+    return response.status(500).json({
+      status: "error",
+      message: `Internal server error - ${err.message}`,
+    });
+  }
+);
 
 server.get("/", (request, response) => {
   response.json({ message: "ok" });
