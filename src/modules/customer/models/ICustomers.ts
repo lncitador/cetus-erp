ff0c6069ff351aa3aac41290d5dcec1ac671810f
lncitador <walaffonofre@icloud.com>
2021-08-07 20:35:30 -0300

    ✨ Create migration and model of customers

---
+++ src/modules/customer/models/ICustomers.ts
@@ -0,0 +1,10 @@
+export default interface ICustomers {
+  id?: string;
+  first_name: string;
+  last_name: string;
+  email: string;
+  password: string;
+  avatar: string;
+  created_at: Date;
+  updated_at: Date;
+}
---
+++ src/shared/infra/typeorm/migrations/1628376926874-CreateCustomer.ts
@@ -0,0 +1,54 @@
+import { MigrationInterface, QueryRunner, Table } from "typeorm";
+
+export class CreateCustomer1628376926874 implements MigrationInterface {
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.createTable(
+      new Table({
+        name: "customers",
+        columns: [
+          {
+            name: "id",
+            type: "uuid",
+            isPrimary: true,
+            generationStrategy: "uuid",
+            default: "uuid_generate_v4()",
+          },
+          {
+            name: "first_name",
+            type: "varchar",
+          },
+          {
+            name: "last_name",
+            type: "varchar",
+          },
+          {
+            name: "email",
+            type: "varchar",
+          },
+          {
+            name: "password",
+            type: "varchar",
+          },
+          {
+            name: "avatar",
+            type: "varchar",
+          },
+          {
+            name: "created_at",
+            type: "timestamp",
+            default: "now()",
+          },
+          {
+            name: "updated_at",
+            type: "timestamp",
+            default: "now()",
+          },
+        ],
+      })
+    );
+  }
+
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.dropTable("customers");
+  }
+}
