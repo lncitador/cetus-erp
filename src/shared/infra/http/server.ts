07cd7ecb02c425f445384a00f8b5600f52e66203
lncitador <walaffonofre@icloud.com>
2021-08-07 18:49:13 -0300

    🔧 continuing the configuration orm

--- src/shared/infra/http/server.ts
+++ src/shared/infra/http/server.ts
@@ -1,5 +1,8 @@
+import "reflect-metadata";
 import express, { Request, Response, NextFunction } from "express";
 import "express-async-errors";
+
+import "@shared/infra/typeorm";
 
 import { AppError } from "@shared/errors/AppError";
 
