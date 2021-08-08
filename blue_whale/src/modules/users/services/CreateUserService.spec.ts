78a7b09293da33e1474ff07303d072256559632f
lncitador <walaffonofre@icloud.com>
2021-08-08 19:51:56 -0300

    ✅ pass Create User 1/1

--- blue_whale/src/modules/users/services/CreateUserService.spec.ts
+++ blue_whale/src/modules/users/services/CreateUserService.spec.ts
@@ -1,4 +1,5 @@
 import { FakerUsersRepository } from "../fakes/repositories/FakerUsersRepository";
+import { IUsersDTO } from "../repositories/IUsersRepository";
 import { CreateUserService } from "./CreateUserService";
 
 let fakerUsersRepository: FakerUsersRepository;
@@ -8,5 +9,18 @@ describe("Create User", () => {
   beforeEach(() => {
     fakerUsersRepository = new FakerUsersRepository();
     createUserService = new CreateUserService(fakerUsersRepository);
+  });
+
+  it("should be able create user", async () => {
+    const data: IUsersDTO = {
+      first_name: "John",
+      last_name: "Doe",
+      email: "johndoe@mail.com",
+      password: "123456",
+    };
+
+    const user = await createUserService.execute(data);
+
+    expect(user.email).toBe("johndoe@mail.com");
   });
 });
